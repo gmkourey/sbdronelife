ada744e3d6757810e3f62780ea7d49d518a6f505
gkourey <grant.kourey@gmail.com>
2018-12-04 09:00:38 -0800

    cleaned up code and added comments

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,8 +1,8 @@
+// Check window height and content body height
 let windowHeight = $(window).height();
 let bodyHeight = $("body").height();
 
-console.log(windowHeight, bodyHeight);
-
+// If window height is bigger than body height then make the header sticky, if not, keep static
 if(windowHeight >= bodyHeight) {
     $("#footer").css("position", "fixed");
     $("#footer").css("bottom", "0px");
@@ -10,6 +10,7 @@ if(windowHeight >= bodyHeight) {
     $("#footer").css("position", "static");
 }
 
+// Take new measurements and do the above function every time the window is resized (i.e. mobile device flipped)
 $(window).resize(function () {
     windowHeight = $(window).height();
     bodyHeight = $("body").height();
--- assets/scripts/overlayMove.js
+++ assets/scripts/overlayMove.js
@@ -1,15 +1,25 @@
+// The overlay on the home page above the video is absolute, and therefore doesn't move 
+// when the navbar opens on mobile devices
+
+// Get the original header height and get the current top measurement for the overlay on load
 let navOriginalSize = $("#header").height();
 let topOverlayAlignment = $(".overlay").css("top").slice(0,3);
 
+// Take these measurements everytime the window resizes (i.e. a mobile device is turned)
 $(window).resize(function () {
     navOriginalSize = $("#header").height();
     topOverlayAlignment = $(".overlay").css("top").slice(0,3);
 })
 
+// On the click of the "hamburger" the function triggers
 $(".navbar-toggler").on("click", function () {
+    // Have a set timeout so it can measure when the navbar is fully expanded
     setTimeout(function () {
+        // Takes difference of the new navbar height and the original height 
         const changeInNavHeight = $("#header").height() - navOriginalSize;
+        // Adds the current "top" measurement to the change in navbar height
         const newTop = (parseInt(topOverlayAlignment) + parseInt(changeInNavHeight)) + "px";
+        // Applies the new measurement to the overlay so it drops down the same height as the navbar extended
         $(".overlay").css("top", newTop);
     }, 350)
 })
